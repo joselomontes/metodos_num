e150497fb5c0b34c4310ddef876a7a6401f4ec71
JoseloMontes <joselo.montes.17@gmail.com>
2021-04-29 03:41:54 -0500

    Se hacen cambios para merge

--- MenuMetodosN/MenuMetodosN/Form1.vb
+++ MenuMetodosN/MenuMetodosN/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         hideSubMenu()
+        'Son cambios muy sencillos
     End Sub
 
     Private Sub hideSubMenu()
